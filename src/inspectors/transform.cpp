687c2157ebf017d4bb4d83a0b13c66ade9a1995a
Daril Rodriguez <darilrodriguez.2@gmail.com>
2024-07-08 02:15:55 -0300

    fix warnings

--- src/inspectors/transform.cpp
+++ src/inspectors/transform.cpp
@@ -9,7 +9,7 @@ void TransformInspector::on_inspector()
 {
     Transform transform;
 
-    ui::InputVector3("Position", &transform.position.x, 0.1f, NULL, NULL, ImGuiSliderFlags_None);
-    ui::InputVector3("Rotation", &transform._euler_angles.x, 0.1f, NULL, NULL, ImGuiSliderFlags_None);
-    ui::InputVector3("Scale", &transform.scale.x, 0.1f, NULL, NULL, ImGuiSliderFlags_None);
+    ui::InputVector3("Position", &transform.position.x, 0.1f, 0, 0, ImGuiSliderFlags_None);
+    ui::InputVector3("Rotation", &transform._euler_angles.x, 0.1f, 0, 0, ImGuiSliderFlags_None);
+    ui::InputVector3("Scale", &transform.scale.x, 0.1f, 0, 0, ImGuiSliderFlags_None);
 }
